c95f76070abf0aa73324512a78fd029882647581
ChihHsuan-Yang <63023138+ChihHsuan-Yang@users.noreply.github.com>
2024-05-07 17:04:03 -0500

    correct

--- MolTransformer/generative/generative_method.py
+++ MolTransformer/generative/generative_method.py
@@ -451,6 +451,7 @@ class GenerateMethods(IndexConvert):
                 molecules_generation_record['Property'].append(max_neighbor_properties)
                 molecules_generation_record['SELFIES'].append(top_k_neighbors['SELFIES'][index_max_neighbor_properties])
                 current_property = max_neighbor_properties
+                current_smile = top_k_neighbors['SMILES'][index_max_neighbor_properties]
                 if self.save:
                     plot_molecules(top_k_neighbors['SMILES'][index_max_neighbor_properties],path = optimistic_property_driven_molecules_generation_report_path + 'step_'+ str(step+1))
                 
